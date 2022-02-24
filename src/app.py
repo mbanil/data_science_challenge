d6225b4bea2cd42817ff5489ff73b498d3b60c4e
Anil Kumar Mysore Badarinarayana <anil_kumar.mysore_badarinarayana@mailbox.tu-dresden.de>
2022-02-24 19:34:26 +0100

    clean

--- src/app.py
+++ src/app.py
@@ -11,48 +11,28 @@ from pyspark.ml.feature import VectorAssembler
 from utils.utils import data_preprocessing
 import requests
 
-
 from flask import Flask, render_template, request
 
-
-
 app = Flask(__name__)
-
 
 spark = SparkSession.builder.appName("Zeiss_predict").getOrCreate()
 args = None
 
-
 @app.route('/')
 def index():
     return render_template('/index.html')
-    # return "OK"
-
+ 
 
 @app.route('/predict-fraud', methods = ['GET'])
 def prediction_product(data=None):
 
-    # print(requests.utils.unquote(data))
-    # print(data)
-
     args = parse_args()
 
     data_json = request.args.get('data')
-
-    # print(data_json)
-
-
-
-    data = '{"months_as_customer":{"0":328},"age":{"0":48},"policy_number":{"0":521585},"policy_bind_date":{"0":1413504000000},"policy_state":{"0":"OH"},"policy_csl":{"0":"250\\/500"},"policy_deductable":{"0":1000},"policy_annual_premium":{"0":1406.91},"umbrella_limit":{"0":0},"insured_zip":{"0":466132},"insured_sex":{"0":"MALE"},"insured_education_level":{"0":"MD"},"insured_occupation":{"0":"craft-repair"},"insured_hobbies":{"0":"sleeping"},"insured_relationship":{"0":"husband"},"capital-gains":{"0":53300},"capital-loss":{"0":0},"incident_date":{"0":1422144000000},"incident_type":{"0":"Single Vehicle Collision"},"collision_type":{"0":"Side Collision"},"incident_severity":{"0":"Major Damage"},"authorities_contacted":{"0":"Police"},"incident_state":{"0":"SC"},"incident_city":{"0":"Columbus"},"incident_location":{"0":"9935 4th Drive"},"incident_hour_of_the_day":{"0":5},"number_of_vehicles_involved":{"0":1},"property_damage":{"0":"YES"},"bodily_injuries":{"0":1},"witnesses":{"0":2},"police_report_available":{"0":"YES"},"total_claim_amount":{"0":71610},"injury_claim":{"0":6510},"property_claim":{"0":13020},"vehicle_claim":{"0":52080},"auto_make":{"0":"Saab"},"auto_model":{"0":"92x"},"auto_year":{"0":2004},"fraud_reported":{"0":"Y"}}'
-    print(args.store_schema)
-
     
-    
-    a_json = json.loads(data)
+    a_json = json.loads(data_json)
     df_pandas = pd.DataFrame.from_dict(a_json)
     df_spark = spark.createDataFrame(df_pandas)
-
-    print(df_spark.show())
 
     df_processed = data_preprocessing(df_spark, args)
     
@@ -67,8 +47,6 @@ def prediction_product(data=None):
     predictions_json = predictionsDF_pandas.to_json()
 
     return predictions_json
-
-
 
 
 def parse_args():
@@ -95,20 +73,11 @@ def parse_args():
     parser.add_argument("--port", type=int,
                         default=8080)
     
-
-    # parser.add_argument(
-    #     "--model-dir",
-    #     type=pathlib.Path,
-    #     default=pathlib.Path("../../models/inference/ml-model"),
-    #     help="dir to save models",
-    # )
     return parser.parse_args()
 
 
 def start_server(host, port):
     app.run(host=host, port=port, debug=True)
-
-
 
     
 def main():
