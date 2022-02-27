c92cdee2500d8028344a96cbab55c10c3cddc65a
Anil Kumar Mysore Badarinarayana <anil_kumar.mysore_badarinarayana@mailbox.tu-dresden.de>
2022-02-27 13:20:40 +0100

    cleaned

--- src/app.py
+++ src/app.py
@@ -23,7 +23,7 @@ def index():
 
 
 @app.route('/predict-fraud', methods = ['GET'])
-def prediction_product(data=None):
+def predict_fraud(data=None):
    
    try:
       args = parse_args()
@@ -81,18 +81,18 @@ def prediction_product(data=None):
 def parse_args():
     
    parser = argparse.ArgumentParser('Flask Service')
-   parser.add_argument("--loglevel", type=str, default="INFO")
    parser.add_argument("--columns_to_drop", type=list, 
                      default=['policy_number','policy_bind_date','policy_state','insured_zip','incident_location',
                      'incident_date','auto_make','auto_model','insured_occupation','age', 'total_claim_amount'])
    parser.add_argument("--columns_to_encode", type=list, 
                      default=['policy_csl', 'insured_sex', 'insured_education_level','insured_hobbies', 'insured_relationship',
                      'incident_type', 'incident_severity','authorities_contacted', 'incident_state', 'incident_city','collision_type'])
+   
    parser.add_argument("--preprocess_hobbies", type=bool, default=True)
    parser.add_argument("--store_schema", type=bool, default=False)
+   
    parser.add_argument("--schema_path", type=str, default="/src/data_repo/feature_store/schema.pkl")
-
-   parser.add_argument("--best_hyper_params_filepath", type=str, default='/src/data/best_hyper_params')
+   parser.add_argument("--best_hyper_params_filepath", type=str, default='/src/data_repo/best_hyper_params/best_hyper_parameters.pkl')
    parser.add_argument("--model_path", type=str, default='/src/model/model.pkl')
    
    return parser.parse_args()
